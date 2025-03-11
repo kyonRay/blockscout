579931ee0a9c848c5641be69da9bdc6ad134c4d8
YE WEIBIN <yeshinebenny@gmail.com>
2025-03-11 11:15:51 +0800

    <deploy>(*): add shell and template

---
+++ package/package.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+export IMAGE_NAME=blockscout-backend
+
+rm -rf output
+rm -f ${IMAGE_NAME}.tar.gz
+mkdir output
+mkdir output/envs
+mkdir output/proxy
+mkdir output/services
+
+export POSTGRES_STATS_DB=stats
+export POSTGRES_STATS_USER=stats
+export POSTGRES_STATS_PASSWORD=n0uejXPl61ci6ldCuE2gQU5Y
+export POSTGRES_BLOCKSCOUT_DB=blockscout
+export POSTGRES_BLOCKSCOUT_USER=blockscout
+export POSTGRES_BLOCKSCOUT_PASSWORD=ceWb1MeLBEeOIfk65gU8EjF8
+envsubst < .env.template > ./output/.env
+
+cp -r ../docker-compose/testnet.yml ./output/testnet.yml
+cp -r ../docker-compose/envs/* ./output/envs
+cp -r ../docker-compose/proxy/* ./output/proxy
+cp -r ../docker-compose/services/*.yml ./output/services
+
+docker compose -f ../docker-compose/testnet.yml build
+docker save -o ./output/"${IMAGE_NAME}".tar blockscout/blockscout:latest
+
+envsubst < start.sh.template > ./output/start.sh
+envsubst < stop.sh.template > ./output/stop.sh
+chmod +x ./output/stop.sh
+
+tar -czf output.tar.gz -C ./output .
