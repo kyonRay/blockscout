202b6ef91f5a33ca1c496b72d930da4a9c4b413d
YE WEIBIN <yeshinebenny@gmail.com>
2025-03-12 10:54:50 +0800

    <deploy>(*): add fisco.yml and set environment variables

--- package/package.sh
+++ package/package.sh
@@ -14,14 +14,18 @@ export POSTGRES_STATS_PASSWORD=n0uejXPl61ci6ldCuE2gQU5Y
 export POSTGRES_BLOCKSCOUT_DB=blockscout
 export POSTGRES_BLOCKSCOUT_USER=blockscout
 export POSTGRES_BLOCKSCOUT_PASSWORD=ceWb1MeLBEeOIfk65gU8EjF8
+export ETHEREUM_JSONRPC_HTTP_URL=https://rpc.eightart.hk
+export ETHEREUM_JSONRPC_TRACE_URL=https://rpc.eightart.hk
+export CHAIN_ID=20200
+export API_V2_ENABLED=true
 envsubst < .env.template > ./output/.env
 
-cp -r ../docker-compose/testnet.yml ./output/testnet.yml
+cp -r ../docker-compose/fisco.yml ./output/fisco.yml
 cp -r ../docker-compose/envs/* ./output/envs
 cp -r ../docker-compose/proxy/* ./output/proxy
 cp -r ../docker-compose/services/*.yml ./output/services
 
-docker compose -f ../docker-compose/testnet.yml build
+docker compose -f ../docker-compose/fisco.yml build
 docker save -o ./output/"${IMAGE_NAME}".tar blockscout/blockscout:latest
 
 envsubst < start.sh.template > ./output/start.sh
